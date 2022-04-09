539e01cea45b88e118c0561607859493a3174e40
Ilya <ilya@sinyugin.ru>
2022-04-09 23:33:33 +0200

    implemented retrieving addresses received

--- bitcoin.py
+++ bitcoin.py
@@ -3,8 +3,22 @@
 
 import pandas 
 
-wallet = str(input("Enter the wallet address: "))
+#wallet = str(input("Enter the wallet address: "))
+wallet = "bc1q57xhcvleefxz4m2w8ytc6nuy76m4wtjxsw66q6"
 transaction_url = 'https://blockchain.info/rawaddr/' + wallet
 df = pandas.read_json(transaction_url)
 transactions=df["txs"]
-print(transactions)
+#print(transactions)
+
+# saving only the addresses where the given wallet has received crypto from 
+# and saving them in a list
+addresses_received = []
+for i in transactions: 
+   # if wallet != i["inputs"][0]["addr"]:
+    for j in i["inputs"]:
+        addresses_received.append(i["inputs"][0]["prev_out"]["addr"])
+
+print("Addresses received: ", addresses_received)
+# with open('transactions.txt', 'w') as f:
+#     for item in transactions:
+#         f.write("%s\n" % item)
